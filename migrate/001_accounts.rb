a334ee5211a17a3246bccf8414faac809d3a3bc2
Stephanie Wilkinson <what.happens@gmail.com>
2023-06-02 10:48:53 -0700

    added new platform to lockfile

---
+++ migrate/001_accounts.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../lib/db'
+
+DB.create_table(:accounts) do
+  primary_key :id
+  File :profile_picture
+  String :email
+  String :password_hash, null: false
+  Time :created_on, { default: Time.now.utc, null: false }
+end
